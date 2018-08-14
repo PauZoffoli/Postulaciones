f2635dd7a8198c44807f2b4817b12154989f0771
PauZoffoli <pauzoffoli@gmail.com>
2018-08-13 23:46:48 -0300

    Antes de php artisan vendor:publish
    Se agregan los archivos de InfyOm, pero aún no se ejecuta el comando php artisan vendor:publish

--- config/app.php
+++ config/app.php
@@ -160,6 +160,16 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        /*SCAFFOLD CUSTOM*/
+        /*SCAFFOLD CUSTOM*/
+        /*SCAFFOLD CUSTOM*/
+        Collective\Html\HtmlServiceProvider::class,
+        Laracasts\Flash\FlashServiceProvider::class,
+        Prettus\Repository\Providers\RepositoryServiceProvider::class,
+        \InfyOm\Generator\InfyOmGeneratorServiceProvider::class,
+        \InfyOm\AdminLTETemplates\AdminLTETemplatesServiceProvider::class, 
+
     ],
 
     /*
@@ -208,6 +218,14 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+
+        /*SCAFFOLD CUSTOM*/
+        /*SCAFFOLD CUSTOM*/
+        /*SCAFFOLD CUSTOM*/
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
+        'Flash'     => Laracasts\Flash\Flash::class,
 
     ],
 
